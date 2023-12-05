e3c248e4704d4473bba1211f87d2d8f73a5e5619
Carlos Estala <estalav@estala.com>
2023-12-04 16:12:43 -0800

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="section tribute">
       <!-- CONTENT START -->
-      <h1 class="title">In Loving Memory</h1>
+      <h1 class="title">En Memoria y celebracion de la Abuela Alicia Estevez</h1>
       <img height="292" src="~/assets/frontimage.jpg" alt="In loving memory" />
       <div class="subtitle"><div class="name">{{ $config.name }}</div>{{ $config.dates }}</div>
       <!-- CONTENT END -->
